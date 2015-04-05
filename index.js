9a7939ab73c604fcde283823c8b353b1e3452194
sashimizakana <atto_999@hotmail.com>
2015-04-05 14:20:17 +0900

    lodash使わないようにした
    ほぼつかってないので外した

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = {
     get:function(alias){
         return emoji[alias] || null;
     }
+    ,getAllAliases:function(){
+        return Object.keys(emoji);
+    }
     ,findAlias:function(code){
         for(var p in emoji){
             if(emoji[p] == code){
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var emojier = require('./index.js');
+var mojier = require('./index.js');
 
 function getCharCode(char){
     return (char.charCodeAt(0) - 0xD800) * 0x400 + char.charCodeAt(1) - 0xDC00 + 0x10000;
@@ -7,10 +7,14 @@ function getCharCode(char){
 
 console.log('---mojier test---');
 console.log('1.get smile');
-var smile = emojier.get('smile');
+var smile = mojier.get('smile');
 assert.ok(getCharCode(smile) == 128516);
 console.log('[ok]');
-console.log('find alias smile');
-assert.ok(emojier.findAlias(smile) == "smile");
+console.log('2.find alias smile');
+assert.ok(mojier.findAlias(smile) == "smile");
 console.log('[ok]');
+console.log('3.get all aliases');
+assert.ok(mojier.getAllAliases().length > 0);
+console.log('[ok]');
+
 console.log('test all ok');
